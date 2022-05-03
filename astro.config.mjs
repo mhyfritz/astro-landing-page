3033bbed2567ae79ac6314aa9d109dccff498452
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2022-05-03 06:31:09 +0200

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
