1cc2ae7ea1833b8732e83fb9186d60f87e2c7650
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2022-05-11 19:53:16 +0200

    set `Astro.site` and fix image url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind()],
   vite: {
     ssr: {
