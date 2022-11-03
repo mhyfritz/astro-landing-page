3a2fec87095ad6233efad014659ff445d86b4659
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2022-11-03 14:04:07 +0100

    make `astro check` happy

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface FeatureItem {
+  description: string;
+  icon: string;
+  title: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
+
+// helper type for astro-eleventy-img
+export interface ImageFormat {
+  srcset: string;
+}
