a122f1663e32f24711af3cd3ac2b6ff4dc2bce47
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2022-11-03 17:00:02 +0100

    set up image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
