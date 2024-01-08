fb40ccba76f5da78d63856a58dd051c244aa8966
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2024-01-08 11:10:43 +0100

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+  integrations: [tailwind(), icon()],
 });
