24502bc5b2202d62b6899e66eceddb173387ae5a
agungbharata <afyastudio@gmail.com>
2024-01-08 12:48:09 +0700

    RajabLP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from 'astro/config';
+
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: squooshImageService(),
+  },
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind()],
   vite: {
