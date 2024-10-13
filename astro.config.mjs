997f5704917a6fdb99cab30e459dafbf617ade81
Alessandro Casazza <imasterale@gmail.com>
2024-10-13 19:03:55 +0200

    Add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://twcrypto.watch/",
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: vercel(),
 });
